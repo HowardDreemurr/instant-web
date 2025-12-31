9b2dd0d174042f15cec07678a1ce779426abadf9
Howard Cui <haoyangcui@outlook.com>
2025-12-31 03:19:23 +0000

    Use relative base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// GitHub Pages friendly:
-// - We use HashRouter so it works without server-side routing config.
 export default defineConfig({
   plugins: [react()],
-  base: "/instant-web/",
+  base: "./",
 });
